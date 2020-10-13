a86e82ddb8806e1dfcdbb942d25d5e974436bcbc
anickel101 <anson.nickel@gmail.com>
2020-10-12 22:28:33 -0400

    solved

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,59 @@
 import React from "react"
 
-const PizzaForm = () => {
-  return(
+class PizzaForm extends React.Component {
+
+  state={
+    id: "",
+    topping: "",
+    size: "",
+    vegetarian: ""
+  }
+
+  submitPizzaEditForm = (e) => {
+    this.props.submitPizzaEditForm(this.state)
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  vegHandler = (e) => {
+    let vegetarian = e.target.value === "true" ? true : false
+    this.setState({vegetarian})
+
+  }
+
+  //set initial conditions of form.
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.pizzaToEdit !== this.props.pizzaToEdit) {
+      this.setState({
+        id: this.props.pizzaToEdit.id,
+        topping: this.props.pizzaToEdit.topping,
+        size: this.props.pizzaToEdit.size,
+        vegetarian: this.props.pizzaToEdit.vegetarian
+      })
+    }
+  }
+
+
+  render() {
+
+    return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input 
+              onChange={this.changeHandler}
+              type="text" 
+              name="topping" 
+              className="form-control" 
+              placeholder="Pizza Topping" 
+              value={this.state.topping}
+              />
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select name="size" onChange={this.changeHandler} value={this.state.size} className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,24 +61,25 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" onChange={this.vegHandler} name="vegetarian" type="radio" value={true} checked={this.state.vegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" onChange={this.vegHandler} name="vegetarian" type="radio" value={false} checked={!this.state.vegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={this.submitPizzaEditForm}>Submit</button>
         </div>
       </div>
 
   )
+  }
 }
 
 export default PizzaForm
